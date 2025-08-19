10ecb3586d7089f116474b65efe0c6f5513a5911
Esteban <estebankaraputny18@gmail.com>
2025-08-19 18:39:21 -0300

    :sparkles: Add: new fictures

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,17 +15,17 @@ const Header = () => {
 
 
         <header className="p-4 w-screen h-screen relative z-10 text-white">
-        <nav className="mt-2">
-            <ul className="flex space-x-4">
-            <li><a href="/" className="hover:underline">Inicio</a></li>
-            <li><a href="/about" className="hover:underline">Saber más</a></li>
-            <li><a href="/contact" className="hover:underline">Comprar</a></li>
+        <nav className="mt-2 flex">
+            <ul className="flex space-x-4 w-full justify-center">
+            <li><a href="/" className="hover:underline p-10">Inicio</a></li>
+            <li><a href="/about" className="hover:underline p-10">Saber más</a></li>
+            <li><a href="/contact" className="hover:underline p-10">Comprar</a></li>
             </ul>
         </nav>
-        <div>
-            <div>
-                <h1 className="text-2xl font-bold">INCENDIARSE</h1>
-                <h2>SIN CONSUMIRSE</h2>
+        <div className='flex justify-center h-full sm:flex-col sm:w-full md:w-4/5 lg:w-3/5 lg:flex-row xl:w-2/5 mx-auto'>
+            <div className='w-2/5'>
+                <h1 className="text-8xl font-bold font-oswald">INCENDIARSE</h1>
+                <h2 className='text-7xl'>SIN CONSUMIRSE</h2>
                 <p>
                     Descubri una nueva forma de ver el mundo a través de mi historia.
                 </p>
@@ -34,7 +34,7 @@ const Header = () => {
                     <a href="">Saber más</a>
                 </div>
             </div>
-            <div>
+            <div className="flex flex-wrap justify-center mt-8 w-3/5">
                 <img src={LibroF} alt="Book Cover" className="w-32 h-48 object-cover mt-4" />
                 <img src={LibroK} alt="" className="w-32 h-48 object-cover mt-4"/>
                 <img src={LibroM} alt="" className="w-32 h-48 object-cover mt-4"/>
